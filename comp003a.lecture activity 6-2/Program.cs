0a8b1db264ab7002304612a0f48ad33668671094
Your Name <you@example.com>
2025-02-17 21:12:03 -0800

    Add project files.

---
+++ comp003a.lecture activity 6-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace comp003a.lecture_activity_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ comp003a.lecture activity 6-2/ShapeCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comp003a.lecture_activity_6_2
+{
+    class ShapeCalculator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+
+        pu
+    }
+}
